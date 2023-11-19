7fe7f361c1d5cccb8d8b81fecb2acd99a6eec891
Komarov23 <komarov2004qw@gmail.com>
2023-11-19 17:08:12 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using lr10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lr10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterConsultation(Registration model)
+        {
+            if (!ModelState.IsValid) return View("Index", model);
+            return RedirectToAction("Confirmation");
+        }
+
+        public IActionResult Confirmation ()
+        {
+            return View("Confirmation");
+        }
+    }
+}
---
+++ Models/Registration.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lr10.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Please select a preferred consultation date")]
+        [FutureDate(ErrorMessage = "Consultation date must be in the future")]
+        [NotOnWeekend(ErrorMessage = "Consultation cannot be on a weekend")]
+        public required DateTime ConsultationDate { get; set; }
+
+        [Required(ErrorMessage = "Please select a product for consultation")]
+        [OnlyOnMonday("ConsultationDate", ErrorMessage = "Only on Monday")]
+        public required string SelectedProduct { get; set; }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value == null) return false;
+
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date)) return date > DateTime.Now;
+            return false;
+        }
+    }
+
+    public class NotOnWeekendAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value == null) return false;
+
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date)) return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+            return false;
+        }
+    }
+
+    public class OnlyOnMonday : ValidationAttribute
+    {
+        private readonly string _otherProperty;
+
+        public OnlyOnMonday(string otherProperty)
+        {
+            _otherProperty = otherProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var property = validationContext.ObjectType.GetProperty(_otherProperty);
+
+            if (property == null) throw new ArgumentException("Property with this name not found");
+
+            var date = property.GetValue(validationContext.ObjectInstance, null);
+
+            if (date is DateTime consultationDate && value is string selectedProduct && selectedProduct == "Basics")
+            {
+                if (consultationDate.DayOfWeek == DayOfWeek.Monday)
+                {
+                    return new ValidationResult("Consultation regarding 'Basics' cannot be scheduled on Monday");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
